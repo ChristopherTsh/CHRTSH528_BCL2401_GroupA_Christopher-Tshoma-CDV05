4b7e376d6af725ff4b81e6821fb70d0d26e5dd90
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-05-30 15:53:53 +0200

    "Updated index.html with new section, added JavaScript event listeners, and modified CSS styles."

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-document
-.getElementById("mobile-menu-toggle")
-.addEventListener("click", function() {
-document.getElementById("mobile-menu").classList.toggle("show");
-} );
+// document
+// .getElementById("mobile-menu-toggle")
+// .addEventListener("click", function() {
+// document.getElementById("mobile-menu").classList.toggle("show");
+// } );
+
+document.getElementById('revealButton').addEventListener('click', function() {
+  const card = document.getElementById('card');
+  if (card.style.display === 'none' || card.style.display === '') {
+      card.style.display = 'flex';
+  } else {
+      card.style.display = 'none';
+  }
+});
